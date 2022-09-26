8ef62481ae7678fd8e35e0824b1520339efdb748
spotdemo4 <racierjet@hotmail.com>
2022-09-25 21:46:08 -0400

    feat: react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import React, { useState, useEffect } from 'react'
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
 
+  return (
+    <div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+// Home
+function Home() {
+
+  //Gets data from /api/hello endpoint, sets it to data variable.
   const [data, setData] = useState([{}])
 
   useEffect(() => {
@@ -16,15 +33,31 @@ function App() {
   }, [])
 
   return (
-    <div>
-
-      {(typeof data.name === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>Here's some data retrieved from the backend:</p>
         <><p>{data.name}</p><p>{data.response}</p></>
-      )}
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  )
+}
 
-    </div>
+// About
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>We are group 3!</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
   )
 }
 
